1a8a3ed954012b0a35e8d6c3d2a2ccf04bb1e6fb
kimch_12i840a <kimch_12i840a@DESKTOP-97IDB48>
2023-04-07 23:55:51 +0900

    김준영 : Coding Test 5개 완료.

---
+++ CodingTest/020_배열 원소의 길이/020_배열 원소의 길이.cpp
@@ -0,0 +1,37 @@
+﻿//문제 설명
+//문자열 배열 strlist가 매개변수로 주어집니다.strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+//
+//제한사항
+//1 ≤ strlist 원소의 길이 ≤ 100
+//strlist는 알파벳 소문자, 대문자, 특수문자로 구성되어 있습니다.
+//입출력 예
+//strlist	                            result
+//["We", "are", "the", "world!"]        [2, 3, 3, 6]
+//["I", "Love", "Programmers."]         [1, 4, 12]
+//입출력 예 설명
+//입출력 예 #1
+//
+//["We", "are", "the", "world!"]의 각 원소의 길이인[2, 3, 3, 6]을 return합니다.
+//입출력 예 #2
+//
+//["I", "Love", "Programmers."]의 각 원소의 길이인[1, 4, 12]을 return합니다.
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// strlist_len은 배열 strlist의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int* solution(const char* strlist[], size_t strlist_len) 
+{
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    int* answer = (int*)malloc(sizeof(int)*strlist_len);
+    strlist
+    return answer;
+}
+
+int main()
+{
+    
+}
+
